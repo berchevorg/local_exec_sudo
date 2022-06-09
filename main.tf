a55956eacccdad13bf56ad11330e04feab61cd43
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-06-09 14:39:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
+# resource "null_resource" "files" {
+#   provisioner "local-exec" {
+#     command = "sudo apt-get update && sudo apt-get install -y tree"
+#   }
+#   triggers = {
+#     run_every_time = uuid()
+#   }
+# }
+
 resource "null_resource" "files" {
   provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get install -y tree"
+    command = "sudo ls"
   }
   triggers = {
     run_every_time = uuid()
